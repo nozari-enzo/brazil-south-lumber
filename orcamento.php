f7327b84d4b9e1cd4c26c2d7f47a279cc710b80c
nozarienzo <liteonking@gmail.com>
2026-09-22 22:10:28 -0300

    Oculta o honeypot direto no HTML para nao depender do CSS em cache

--- orcamento.php
+++ orcamento.php
@@ -54,8 +54,9 @@
       </div>
 
       <form class="contact-form" id="contact-form" action="enviar.php" method="POST" novalidate>
-        <!-- campo-armadilha anti-spam: display:none impede o preenchimento automático do navegador -->
-        <div class="hp-field" aria-hidden="true">
+        <!-- campo-armadilha anti-spam: display:none impede o preenchimento automático do navegador
+             (inline, para não depender do style.css estar atualizado no cache) -->
+        <div class="hp-field" style="display: none" aria-hidden="true">
           <input type="text" name="campo_extra_kx91" tabindex="-1" autocomplete="off">
         </div>
 
